59840edb9d3b5e11cffd5d4aea50fde2a30642c7
yclee98 <yclee0498@gmail.com>
2022-04-07 01:40:43 +0800

    1. move initalize status to reborn since assert cannot put outside function; 2. some syntax changes to move(); 3. added write to functions for debugging purposes, can remove next time

--- Agent.pl
+++ Agent.pl
@@ -1,10 +1,3 @@
-/*
-working_directory(CWD, 'D:/OneDrive - Nanyang Technological University/AY2S2/CZ3005 ArtificialIntelligence/Lab2').
-consult('Agent.pl').
-reconsult('Agent.pl').
-*/
-
-
 %use asserta to add to beginning and retract to remove
 :-dynamic
     current/3,
@@ -23,11 +16,11 @@ reconsult('Agent.pl').
 
 
 % initialize the status of the objectives and assets of the agent
-assertz(wumpus_alive),
-assertz(agent_alive),
-assertz(has_gold),
-assertz(hasarrow),
-assertz(current(0, 0, rnorth)).
+%asserta(wumpus_alive),
+%asserta(agent_alive),
+%asserta(has_gold),
+%asserta(hasarrow),
+%asserta(current(0, 0, rnorth)).
 
 reborn():-
     retractall(current(_, _, _)),
@@ -41,12 +34,18 @@ reborn():-
     retractall(hasarrow()),
 
     % when reborn, agent loses gold coin
-    retractall(has_gold(agent)),
+    retractall(has_gold),
 
     % Set wumpus back alive
-    assertz(wumpus_alive).
+    asserta(wumpus_alive),
+    asserta(agent_alive),
+    asserta(has_gold),
+    asserta(hasarrow),
+    asserta(current(0, 0, rnorth)).
 
-reposition():- 
+
+
+reposition([Confounded, Stench, Tingle, Glitter, Bump, Scream]):- 
     retractall(current(_, _, _)),
     retractall(visited(_, _)),
     retractall(confoundus(_, _)),
@@ -58,43 +57,57 @@ reposition():-
     asserta(current(0,0,rnorth)),
     asserta(confoundus(0,0)).
 
+
 % A = Forward, TurnLeft, TurnRight
 % D = rnorth, rsouth, reast, rwest
 % L = [Confounded, Stench, Tingle, Glitter, Bump, Scream]
 % 1 = on, 0 = off
 move(A, [Confounded, Stench, Tingle, Glitter, Bump, Scream]):-
+    current(X, Y, _),
+    (Confounded == 1 -> asserta(confoundus(X,Y));true),
+
     update_action(A),
-    update_wumpus(Stench),
-    update_portal(Tingle),
-    update_coin(Glitter).
+    
+    % need to make it true so that if function return false it will still run the code 
+    \+ update_wumpus(Stench) -> true,
+    \+ update_portal(Tingle) -> true,
+    \+ update_coin(Glitter) -> true.
 
-update_action(TurnLeft):-
+update_action(turnleft):-
+    write("turnleft "),
     current(X, Y, D),
-    (D == rnorth -> retract(current(X, Y, D)), assertz(current(X, Y, rwest));
-     D == rsouth -> retract(current(X, Y, D)), assertz(current(X, Y, reast));
-     D == reast -> retract(current(X, Y, D)), assertz(current(X, Y, rnorth));
-     D == rwest -> retract(current(X, Y, D)), assertz(current(X, Y, rsouth))).
+    (D == rnorth -> retract(current(X, Y, D)), asserta(current(X, Y, rwest));
+     D == rsouth -> retract(current(X, Y, D)), asserta(current(X, Y, reast));
+     D == reast -> retract(current(X, Y, D)), asserta(current(X, Y, rnorth));
+     D == rwest -> retract(current(X, Y, D)), asserta(current(X, Y, rsouth))).
 
 
-update_action(TurnRight):-
+update_action(turnright):-
+    write("turnright "),
     current(X, Y, D),
-    (D == rnorth -> retract(current(X, Y, D)), assertz(current(X, Y, reast));
-     D == rsouth -> retract(current(X, Y, D)), assertz(current(X, Y, rwest));
-     D == reast -> retract(current(X, Y, D)), assertz(current(X, Y, rsouth));
-     D == rwest -> retract(current(X, Y, D)), assertz(current(X, Y, rnorth))).
+    (D == rnorth -> retract(current(X, Y, D)), asserta(current(X, Y, reast));
+     D == rsouth -> retract(current(X, Y, D)), asserta(current(X, Y, rwest));
+     D == reast -> retract(current(X, Y, D)), asserta(current(X, Y, rsouth));
+     D == rwest -> retract(current(X, Y, D)), asserta(current(X, Y, rnorth))).
 
 
-update_action(Forward):-
-    % get orientation D
+update_action(moveforward):-
+    write("forward "),
     current(X, Y, D),
-
-    (D == rnorth -> retract(current(X, Y, D)), assertz(current(X, Y+1, D));
-     D == rsouth -> retract(current(X, Y, D)), assertz(current(X, Y-1, D));
-     D == reast -> retract(current(X, Y, D)), assertz(current(X+1, Y, D));
-     D == rwest -> retract(current(X, Y, D)), assertz(current(X-1, Y, D))).
+    asserta(visited(X,Y)), %make old position as visited before moving forward
+    Z1 is Y + 1,
+    Z2 is Y - 1, 
+    Z3 is X + 1, 
+    Z4 is X - 1, 
+    
+    (D == rnorth -> retract(current(X, Y, D)), asserta(current(X, Z1, D));
+     D == rsouth -> retract(current(X, Y, D)), asserta(current(X, Z2, D));
+     D == reast -> retract(current(X, Y, D)), asserta(current(Z3, Y, D));
+     D == rwest -> retract(current(X, Y, D)), asserta(current(Z4, Y, D))).
 
 
 update_wumpus(0):-
+    write("stench0 "),
     current(X,Y,D),
 
     % if stench is not perceived, wumpus cannot be in adj rooms
@@ -106,10 +119,11 @@ update_wumpus(0):-
 
 
 update_wumpus(1):-
+    write("stench1 "),
     current(X,Y,D),
-    stench(X,Y),
+    asserta(stench(X,Y)),
 
-    % if percieved smelly, update KB that wumpus MAY be in one of the adj rooms
+    % if percieved stench, update KB that wumpus MAY be in one of the adj rooms
     Z1 is Y + 1, determine_wumpus(X, Z1),
     Z2 is Y - 1, determine_wumpus(X, Z2),
     Z3 is X + 1, determine_wumpus(Z3, Y),
@@ -117,6 +131,7 @@ update_wumpus(1):-
 
 
 update_portal(0):-
+    write("tingle0 "),
     current(X,Y,D),
 
     % if tingle is not perceived, portal cannot be in adj rooms
@@ -128,29 +143,34 @@ update_portal(0):-
 
 % if perceived tingle, update KB that portal MAY be in one of the adj rooms
 update_portal(1):-
+    write("tingle1 "),
     current(X,Y,D),
-    tingle(X,Y),
+    asserta(tingle(X,Y)),
 
     % if tingle is perceived, portal MAY be in adj rooms
-    Z1 is Y + 1, assertz(confoundus(X, Z1)),
-    Z2 is Y - 1, assertz(confoundus(X, Z2)),
-    Z3 is X + 1, assertz(confoundus(Z3, Y)),
-    Z4 is X - 1, assertz(confoundus(Z4, Y)).
+    Z1 is Y + 1, (\+confoundus(X,Z1) -> asserta(confoundus(X, Z1)); true),
+    Z2 is Y - 1, (\+confoundus(X,Z2) -> asserta(confoundus(X, Z2)); true),
+    Z3 is X + 1, (\+confoundus(Z3,Y) -> asserta(confoundus(Z3, Y)); true),
+    Z4 is X - 1, (\+confoundus(Z4,Y) -> asserta(confoundus(Z4, Y)); true).
 
 
 % if percieve glitter, cell is inhabited by coin
 update_coin(1):-
+    write("glitter1 "),
     current(X,Y,D),
-    glitter(X,Y).
+    asserta(glitter(X,Y)).
 
 
 % find overlapping wumpus(X,Y) rooms in KB (previously existed), wumpus may be in those overlapping rooms
 determine_wumpus(X,Y):-
+    write("determine wumpus "),
     % SYNTAX: (cond -> if-func ; else-func), \+ : Negation
     % if: there is prev wumpus(X,Y) entry with the exact same coords, More likely wumpus is in that room(s) -> already recorded
     % else: if there is no prior entry, add the new entry
 
-    (\+wumpus(X,Y) -> assertz(wumpus(X, Y)) : ).
+    (\+wumpus(X,Y) -> asserta(wumpus(X, Y)); true).
 
-explore(L):-
+
+
+%explore(L):-
 
